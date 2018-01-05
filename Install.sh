cca54f2b9e351b4866cbaf35252fa2546f0e2406
Jian.Xue <xander0731@163.com>
2018-01-04 22:21:42 -0800

    20180105-1421

--- Install.sh
+++ Install.sh
@@ -13,28 +13,28 @@ ln -s $(pwd)/vim ~/.vim
 
 # Install Vundle
 cd vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 
 # Install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/scrooloose/nerdtree.git vim/bundle/nerdtree
 
 # Install indentLine
-git clone https://github.com/Yggdroot/indentLine.git ~/.vim/bundle/indentLine
+git clone https://github.com/Yggdroot/indentLine.git vim/bundle/indentLine
 
 # Install powerline
 sudo apt-get -y install powerline
 
 # Install airline
-git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline.git vim/bundle/vim-airline
 
 # Install ctags
 sudo apt-get -y install ctags
 
 # Install tagbar
-git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
+git clone https://github.com/majutsushi/tagbar.git vim/bundle/tagbar
 
 # Install vim-commentary
-git clone git://github.com/tpope/vim-commentary.git
+git clone git://github.com/tpope/vim-commentary.git vim/bundle/vim-commentary
 
 # Install rainbow
-git clone git://github.com/luochen1990/rainbow.git
+git clone git://github.com/luochen1990/rainbow.git vim/bundle/rainbow
